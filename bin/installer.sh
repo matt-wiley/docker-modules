cc6189a0ec844955a8637f9196f3a4c70e2b5af8
Matt Wiley <mattwiley+gitlab@fastmail.com>
2021-10-28 14:22:38 -0400

    Better organized modules. Include installer script.

---
+++ bin/installer.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function dockerModuleInstaller {
+
+    function banner {
+        # Helper function to pretty print banner messages in build log
+        printf " # \n # ${1}\n # \n"
+    }
+
+    case "${1}" in 
+
+        "download-scripts")
+            shift;
+            
+            banner "Creating modules directory"
+            mkdir -p modules
+            cd modules
+
+            banner "Downloading module scripts"
+            for moduleName in "$@"; do 
+                echo -n "Downloading module '${moduleName}' ... "
+                curl -sSL "https://raw.githubusercontent.com/matt-wiley/docker-modules/main/modules/${moduleName}.sh"
+                echo "done."
+            done
+            cd ../
+            ;;
+
+        "install")
+            banner "Installing modules"
+            for module in ./modules/*; do 
+                banner "Running install for ${module}"
+                "./${module}" install
+            done 
+
+        *) ;;
+
+    esac 
+
+}
+dockerModuleInstaller "$@"
